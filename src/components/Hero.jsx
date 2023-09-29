a0251f6359799a88c161fabefb6d49bb4e5454a8
Pratham <sinhapratham1234@gmail.com>
2023-09-29 19:35:56 +0530

    Minor mobiile UI changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,47 +1,51 @@
-import GlobeCanvas from './Globe';
-import './Hero.css';
-import Typed from 'react-typed';
+import GlobeCanvas from "./Globe";
+import "./Hero.css";
+import Typed from "react-typed";
 
-const Hero =() =>{
-    return(
-        <div className="Content justify-center flex-col md:justify-between md:flex-row">
-            {/* absolute right-10 top-10 */}
-            <div className='flex'>
-                <div className=' flex flex-col justify-center mt-2 mx-4'>
-                    <span className='w-5 h-5 rounded-full bg-[#3f97f0]'/>
-                    <span className='Line'/>
-
-                </div>
-                <div className='text-white px-2 font-bold pt-5'>
-                    <div className='text-4xl sm:text-5xl'> Myself 
-                        <span className='p-2 text-[#3f97f0]'>Pratham Sinha</span>
-                    </div>
-                    <br/>
-                    <div className='text-2xl md:text-4xl my-3   '>Invested in: </div>
-                    <div>
-                    <Typed className='text-2xl md:text-4xl '
-                        strings={[
-                            'Front-End Web Development',
-                            'Competitive Programming',
-                            'Machine Learning',
-                            'Deep Learning',
-                            'Data Analysis']}
-                        typeSpeed={50}
-                        backSpeed={50}
-                        loop
-                    />
-                    </div>
-                </div>
-            </div>
-            
-            <div className='can-prop p-10'>
-                <GlobeCanvas/>
-            </div>
-            
-        
+const Hero = () => {
+  return (
+    <div className="Content justify-center flex-col md:justify-between md:flex-row">
+      {/* absolute right-10 top-10 */}
+      <div className="flex">
+        <div className=" flex flex-col justify-center mt-2 mx-4">
+          <span className="w-5 h-5 rounded-full bg-[#3f97f0]" />
+          <span className="Line" />
         </div>
-    )
-    
-}
+        <div className="text-white px-2 font-bold pt-5">
+          <div className="text-4xl sm:text-5xl">
+            {" "}
+            Myself
+            <span className="p-2 text-[#3f97f0]">Pratham Sinha</span>
+          </div>
+          <br />
+          <div className="text-2xl md:text-4xl my-3   ">Invested in: </div>
+          <div>
+            <Typed
+              className="text-2xl md:text-4xl "
+              strings={[
+                "Front-End Web Development",
+                "Competitive Programming",
+                "Machine Learning",
+                "Deep Learning",
+                "Data Analysis",
+              ]}
+              typeSpeed={50}
+              backSpeed={50}
+              loop
+            />
+          </div>
+        </div>
+      </div>
 
-export default Hero ;
+      <div>
+        <div className="bg-transparent z-30 md:z-0 absolute h-[350px] w-[350px]"></div>
+
+        <div className="can-prop p-10">
+          <GlobeCanvas />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,58 +1,57 @@
-import './Skill.css'
+import "./Skill.css";
 
-import VanillaTilt from 'vanilla-tilt';
-import Tilt from 'react-parallax-tilt' ;
+import VanillaTilt from "vanilla-tilt";
+import Tilt from "react-parallax-tilt";
 
-const Scard =({logo}) => {
-    return (
-        <Tilt
-            tiltReverse={true}
-            className="parallax-effect-glare-scale"
-            perspective={500}
-            scale={1.02}
-            gyroscope={true}>
-            <div className=' skil-card ' > 
-            {/* <p className='text-white bg-white'>this is logo </p> */}
-                <img src= {logo}  className=' skill-img ' />
-                {console.log("logo") }
-            </div>
-        </Tilt> 
-    )
-}
+const Scard = ({ logo }) => {
+  return (
+    <Tilt
+      tiltReverse={true}
+      className="parallax-effect-glare-scale"
+      perspective={500}
+      scale={1.02}
+      gyroscope={true}
+    >
+      <div className=" skil-card ">
+        {/* <p className='text-white bg-white'>this is logo </p> */}
+        <img src={logo} className=" skill-img " />
+        {/* {console.log("logo") } */}
+      </div>
+    </Tilt>
+  );
+};
 
-const Skill=() =>{
-    return (
-       
-        <div className='skill'>
-            {console.log("hello") }
-            {/* <div className='hidden md:absolute right-10 top-130 md:flex flex-col justify-center m-4 '>
+const Skill = () => {
+  return (
+    <div className="skill">
+      {/* {console.log("hello") } */}
+      {/* <div className='hidden md:absolute right-10 top-130 md:flex flex-col justify-center m-4 '>
                 <span className='w-5 h-5 rounded-full bg-[#3f97f0]'/>
                 <span className='skill-line'/>
 
             </div> */}
 
-            <div className='text-[#3f97f0] text-4xl md:text-5xl font-bold' id='shead'>
-            <div className='mx-3 py-2 w-15 h- 15 px-6 hover:cursor-pointer'> Things I Know :</div>
-            </div>
-
-            <div className=' flex flex-row justify-center item-center '>
-            
-                <div className='flex flex-wrap justify-center p-4 '>
-
-                    <Scard logo= {'logo192.png'}/>
-                    <Scard logo= {'tailwind-css-icon.png'}/>
-                    <Scard logo= {'python.png'} />
-                    <Scard logo={'js.png'} />
-                    <Scard logo={'css-icon.png'} />
-                    <Scard logo={'html-icon.png'} />
-                    <Scard logo={'cpp.png'} />
-
-                </div>
-                
-            </div> 
-            <div className='h-3'></div>
+      <div className="text-[#3f97f0] text-4xl md:text-5xl font-bold" id="shead">
+        <div className="mx-3 py-2 w-15 h- 15 px-6 hover:cursor-pointer">
+          {" "}
+          Things I Know :
         </div>
-    )
-}
+      </div>
 
-export default Skill ;
+      <div className=" flex flex-row justify-center item-center ">
+        <div className="flex flex-wrap justify-center p-4 ">
+          <Scard logo={"logo192.png"} />
+          <Scard logo={"tailwind-css-icon.png"} />
+          <Scard logo={"python.png"} />
+          <Scard logo={"js.png"} />
+          <Scard logo={"css-icon.png"} />
+          <Scard logo={"html-icon.png"} />
+          <Scard logo={"cpp.png"} />
+        </div>
+      </div>
+      <div className="h-3"></div>
+    </div>
+  );
+};
+
+export default Skill;
