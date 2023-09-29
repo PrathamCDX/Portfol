16fb97effa9a61c1ab699b030e8378dcb2e3a6b7
Pratham <sinhapratham1234@gmail.com>
2023-09-29 19:55:31 +0530

    _

--- src/components/Globe.jsx
+++ src/components/Globe.jsx
@@ -1,22 +1,18 @@
-import React, { Suspense } from "react";
+import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
-// import CanvasLoader from "../Loader";
 
 const Globe = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
-  return (
-    <primitive object={earth.scene} scale={3} position-y={0}  />
-  );
+  return <primitive object={earth.scene} scale={3} position-y={0} />;
 };
 
 const GlobeCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 20]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
@@ -31,18 +27,16 @@ const GlobeCanvas = () => {
       <ambientLight />
       <hemisphereLight /> */}
 
-      
-        <OrbitControls
-          autoRotate
-          autoRotateSpeed={10}
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Globe />
+      <OrbitControls
+        autoRotate
+        autoRotateSpeed={10}
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+      />
+      <Globe />
 
-        <Preload all />
-      
+      <Preload all />
     </Canvas>
   );
 };
